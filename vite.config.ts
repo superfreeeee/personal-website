68c9221f6380a9b7dcf80ef403a6c57936833ee0
superfree <superfreeeee@gmail.com>
2024-12-11 00:20:05 +0800

    chore: split chunks

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,16 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes('react-router-dom') || id.includes('react-dom') || id.includes('react')) {
+            return 'lib-react';
+          }
+        },
+      },
+    },
+  },
 });
