04e9f9a2433fcb91bcd6c79f26628d11178d6f43
superfree <superfreeeee@gmail.com>
2024-12-10 10:22:07 +0800

    chore: update react to 19

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import 'boxicons/css/boxicons.min.css';
 
 import { App } from './App';
 
-ReactDOM.render(<App />, document.querySelector('#app'));
+const loadApp = async () => {
+  const app = createRoot(document.querySelector('#app') as HTMLElement);
+  app.render(<App />);
+};
+
+loadApp();
